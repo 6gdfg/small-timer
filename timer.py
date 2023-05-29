4f066ea1a1d4ff1e2fa2f7e523aa298a22ce24bc
6gdfg <115078566+6gdfg@users.noreply.github.com>
2023-05-29 22:04:12 +0800

    create some templates

--- timer.py
+++ timer.py
@@ -5,8 +5,8 @@ import winsound
 class Timer:
     def __init__(self, master):
         self.master = master
-        self.master.title("倒计时")
-        self.master.geometry("700x400")
+        self.master.title("timer")
+        self.master.geometry("900x400")
         self.master.resizable(False, False)
 
         self.hour = 0
@@ -23,23 +23,38 @@ class Timer:
         self.stop_button = tk.Button(self.master, text="stop", font=("Arial", 20), command=self.stop_timer)
         self.stop_button.pack(pady=10)
 
-        self.hour_button = tk.Button(self.master, text="hour+1", font=("Arial", 16), command=self.add_hour)
+        self.hour_button = tk.Button(self.master, text="h+1", font=("Arial", 16), command=self.add_hour)
         self.hour_button.pack(side=tk.LEFT, padx=10)
 
-        self.minute_button = tk.Button(self.master, text="minute+1", font=("Arial", 16), command=self.add_minute)
+        self.minute_button = tk.Button(self.master, text="min+1", font=("Arial", 16), command=self.add_minute)
         self.minute_button.pack(side=tk.LEFT, padx=10)
 
-        self.second_button = tk.Button(self.master, text="second+1", font=("Arial", 16), command=self.add_second)
+        self.second_button = tk.Button(self.master, text="s+1", font=("Arial", 16), command=self.add_second)
         self.second_button.pack(side=tk.LEFT, padx=10)
 
-        self.hour_subtract_button = tk.Button(self.master, text="hour-1", font=("Arial", 16), command=self.subtract_hour)
+        self.hour_subtract_button = tk.Button(self.master, text="h-1", font=("Arial", 16), command=self.subtract_hour)
         self.hour_subtract_button.pack(side=tk.LEFT, padx=10)
 
-        self.minute_subtract_button = tk.Button(self.master, text="minute-1", font=("Arial", 16), command=self.subtract_minute)
+        self.minute_subtract_button = tk.Button(self.master, text="min-1", font=("Arial", 16), command=self.subtract_minute)
         self.minute_subtract_button.pack(side=tk.LEFT, padx=10)
 
-        self.second_subtract_button = tk.Button(self.master, text="second-1", font=("Arial", 16), command=self.subtract_second)
+        self.second_subtract_button = tk.Button(self.master, text="s-1", font=("Arial", 16), command=self.subtract_second)
         self.second_subtract_button.pack(side=tk.LEFT, padx=10)
+        
+        self.one_minute_button = tk.Button(self.master, text="1min", font=("Arial", 16), command=self.set_one_minute)
+        self.one_minute_button.pack(side=tk.LEFT, padx=10)
+
+        self.five_minutes_button = tk.Button(self.master, text="5min", font=("Arial", 16), command=self.set_five_minutes)
+        self.five_minutes_button.pack(side=tk.LEFT, padx=10)
+
+        self.ten_minutes_button = tk.Button(self.master, text="10min", font=("Arial", 16), command=self.set_ten_minutes)
+        self.ten_minutes_button.pack(side=tk.LEFT, padx=10)
+
+        self.thirty_minutes_button = tk.Button(self.master, text="30min", font=("Arial", 16), command=self.set_thirty_minutes)
+        self.thirty_minutes_button.pack(side=tk.LEFT, padx=10)
+
+        self.one_hour_button = tk.Button(self.master, text="1h", font=("Arial", 16), command=self.set_one_hour)
+        self.one_hour_button.pack(side=tk.LEFT, padx=10)
 
     def start_timer(self):
         self.start_button.config(state=tk.DISABLED)
@@ -105,6 +120,35 @@ class Timer:
             self.hour = 0
         self.timer_label.config(text="{}:{}:{}".format(self.hour, self.minute, self.second))
     
+    def set_one_minute(self):
+        self.hour = 0
+        self.minute = 1
+        self.second = 0
+        self.timer_label.config(text="{}:{}:{}".format(self.hour, self.minute, self.second))
+
+    def set_five_minutes(self):
+        self.hour = 0
+        self.minute = 5
+        self.second = 0
+        self.timer_label.config(text="{}:{}:{}".format(self.hour, self.minute, self.second))
+
+    def set_ten_minutes(self):
+        self.hour = 0
+        self.minute = 10
+        self.second = 0
+        self.timer_label.config(text="{}:{}:{}".format(self.hour, self.minute, self.second))
+
+    def set_thirty_minutes(self):
+        self.hour = 0
+        self.minute = 30
+        self.second = 0
+        self.timer_label.config(text="{}:{}:{}".format(self.hour, self.minute, self.second))
+
+    def set_one_hour(self):
+        self.hour = 1
+        self.minute = 0
+        self.second = 0
+        self.timer_label.config(text="{}:{}:{}".format(self.hour, self.minute, self.second))
     def play_ringtone(self):
         frequency = 2500  # Set frequency to 2500 Hz
         duration = 1000  # Set duration to 1000 ms (1 second)
@@ -115,8 +159,3 @@ class Timer:
 root = tk.Tk()
 timer = Timer(root)
 root.mainloop()
-        
-    
-
-
-
